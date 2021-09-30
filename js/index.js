beb93848a5d53e51838f37738b4f95bccf624688
Mélyssa Auberton <melyssa.auberton@gmail.com>
2021-09-30 13:23:11 +0200

    début de modifs pour webpack

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import Arrays from "./model/Arrays.js";
-import EventsManager from "./model/EventsManager.js";
-
-//on crée un tableau normalisé avec les datas pour faciliter les recherches sur le site à son ouverture
-const arrays= new Arrays();
-
-//on instancie la classe eventsManager qui va initialiser tous les events sur l'application et on lui passe le tableau de base en argument
-const events= new EventsManager(arrays.navigationArrayGenerator());
-events.init();
